9f7b586b4b409f0535763ba3d92eec041db77e80
Carop1 <caropmejia22@gmail.com>
2024-01-10 13:23:58 -0500

    Estudio filtro 1

---
+++ main.py
@@ -0,0 +1,26 @@
+import menus.menu_principal as mp
+import os
+
+if __name__ == '__main__':
+    isActiveApp = True
+    opMainMenu =0
+    
+    while (isActiveApp):
+        os.system('cls')
+        mp.generarMainMenu()
+        try:
+            opMainMenu = int(input(":)_"))
+        except ValueError:
+            print("Error en el dato ingresado")
+        else:
+            
+            if(opMainMenu == 1):
+                pass
+            elif (opMainMenu == 2):
+                pass
+            elif (opMainMenu == 3):
+                pass
+            elif (opMainMenu == 4):
+                pass
+            elif (opMainMenu == 5):
+                isActiveApp = False
---
+++ menus/menu_principal.py
@@ -0,0 +1,26 @@
+opciones = ['Registrar nuevo cliente','Borrar Cliente','Editar Cliente','Buscar','salir']
+
+
+
+def generarMainMenu():
+    bandera = False
+    header="""
+    +++++++++++++++++++++++++++++++++++
+    + SISTEMA DE REGISTRO DE CLIENTES +
+    +++++++++++++++++++++++++++++++++++
+    """
+    print(header)
+    
+    for i,item in enumerate(opciones):
+        print(f'{(i+1)} - {item}')    
+    try:
+        opciones_len = int(input("Escriba la opcion del menú que necesita"))
+        if opciones_len <= 5:
+            badera = True
+    except IndexError:
+            print("El valor ingresado es invalido")
+    else:
+            bandera = False
+    
+    
+    return opciones_len, 
